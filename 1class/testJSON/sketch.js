bc3b77ad621483ceb2ea96164fa3fda99a24a560
fraco <fraco.ramirez@gmail.com>
2015-09-30 17:15:13 -0400

    added aparent modifications to testJSON/sketch.js

--- 1class/testJSON/sketch.js
+++ 1class/testJSON/sketch.js
@@ -1,10 +1,15 @@
-var data;
-var describedObjects = [];
 var galleryTextedObjects = [];
-var objWithDescOrGalText = [];
-var notDesOrTexObjs = [];
+
+var data;
 var repeatedDescription = [];
+var describedObjects = [];
 var tNo = '2bc798650125e219377b21f6cfdf5bd5';
+
+var existingDescriptions = [];
+var descriptions = [];
+var colSize;
+
+
 
 function preload() {
   //data = loadJSON('testdata.json');  
@@ -15,34 +20,39 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(600, 10);
-  //createParagraph
-  // createP(data.formats);
-  //println(data.object.justification);
-  //createP(data.getObjects);
-  createP(data.objects[34].gallery_text);
-  // println(data.objects)
-  for(i = 0; i < data.objects.length; i++){
-	data.objects[i];
-	if(data.objects[i].description != null){
-		describedObjects.push(data.objects[i].description); //objects’ index of JSON, id, text
+	createCanvas(600, 10);
+
+
+	for(i = 0; i < data.objects.length; i++){
+
+		if(data.objects[i].description != null){
+
+			if (existingDescriptions[data.objects[i].description] === true) {
+
+			} else {
+				describedObjects.push(data.objects[i]); //objects’ index of JSON, id, text
+				existingDescriptions[data.objects[i].description] = true;
+				// if (existingDescriptions[i] = true){
+					descriptions.push(existingDescriptions[i]);
+				// }
+				// existingDescriptions.push(data.objects[i].description); //= true;			
+
+			}
+			// repeatedDescription.push(data.objects[i].description);
+		}
+
+		// if(data.objects[i].description == 'Sixty-eight blocks used for printing the textile "Flowers after Van Huysum," designed by Harry Wearne. The textile was printed by Stead McAlpin and Company Limited, Cummersdale near Carlisle, England. Although printed in England, the design was confined to the American market and did not become available worldwide until 1949. The minimum unit of the completed pattern would have been forty-two inches high and seventeen inches wide and would repeat as a half-drop pattern.'){
+		// 	repeatedDescription.push(data.objects[i].id);
+		// }
 	}
-	if(data.objects[i].gallery_text != null){
-		galleryTextedObjects.push(data.objects[i].gallery_text); //objects’ index of JSON, id, text
-	}
-	if(data.objects[i].gallery_text != null || data.objects[i].description != null){
-		objWithDescOrGalText.push(data.objects[i].index); //objects’ index of JSON, id, text
-	}
-	if(data.objects[i].description == null && data.objects[i].gallery_text == null){
-		notDesOrTexObjs.push(data.objects[i].index);
-	}
-	if(data.objects[i].description == 'Sixty-eight blocks used for printing the textile "Flowers after Van Huysum," designed by Harry Wearne. The textile was printed by Stead McAlpin and Company Limited, Cummersdale near Carlisle, England. Although printed in England, the design was confined to the American market and did not become available worldwide until 1949. The minimum unit of the completed pattern would have been forty-two inches high and seventeen inches wide and would repeat as a half-drop pattern.'){
-		repeatedDescription.push(data.objects[i].id);
-	}
-} 
+
 }
 
 function draw() {
   background(0);
   // ellipse(200, 100, 50, 50);
+  // colSize = existingDescriptions.length;
+  // createP(existingDescriptions[random(existingDescriptions.length)].description);
+  // createP(existingDescriptions);
+
 }
